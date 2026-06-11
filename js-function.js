302fcd0787500cabc947542dadeedaa517bd47b8
Divyanshu <devdivyanshuvarshney@gmail.com>
2026-06-11 19:46:08 +0530

    created result processing script

---
+++ js-function.js
@@ -0,0 +1,40 @@
+const students=[
+    {
+        name:"Divyanshu",
+        course:"Full Stack Development",
+        marks:[90,80,70,68,59]
+    },
+    {
+        name:"Rohit",
+        course:"Data Science",
+        marks:[85,75,65,55,45]
+    },
+    {
+        name:"Anjali",
+        course:"Machine Learning",
+        marks:[95,85,75,65,55]
+    }   
+]
+/*Problem statement: convert the given objects in array in below format max marks is 500. if percentage is above 60 then result status is pass otherwise fail.:
+[
+    {
+        name:"Divyanshu",
+        course:"Full Stack Development",
+        marks:[90,80,70,68,59],
+        totalMarks:367,
+        percentage:73.4,
+        resultStatus:"Pass"
+    },
+*/
+const updatedStudents=students.map(student=>{
+    const totalMarks=student.marks.reduce((acc,mark)=>acc+mark,0);
+    const percentage=(totalMarks/500)*100;
+    const resultStatus=percentage>60?"Pass":"Fail";
+    return {
+        ...student,
+        totalMarks,
+        percentage,
+        resultStatus
+    }
+});
+console.log(updatedStudents);
